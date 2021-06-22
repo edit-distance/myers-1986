edit-distance/myers-1986

commit ff2a6f8c62a968fa9644140bad996cf93c6205fb

:bicyclist: perf(backwardStep): Refactor hot loop to avoid one addition.

src/backwardStep.js
@@ -39,27 +39,27 @@ export default function backwardStep(center, D, V, eq, li, lj, ri, rj, Delta) {
 		Delta,
 	});
 
-	const cx = center + 1;
-	const cy = center - 1;
+	let c = center + LB;
+	const cMax = center + UB;
 
-	const k = LB;
-	const xp = V[cx + k];
-	const yp = V[cy + k];
-	V[center + k] = k !== -D && xp > yp ? yp : xp - 1;
+	assert(c - center === LB);
+	const xp = V[c + 1];
+	const yp = V[c - 1];
+	V[c] = LB !== -D && xp > yp ? yp : xp - 1;
 
-	for (let k = LB + 2; k < UB; k += 2) {
-		assert(k !== -D && k !== D);
-		const xp = V[cx + k];
-		const yp = V[cy + k];
-		V[center + k] = xp > yp ? yp : xp - 1;
+	for (c += 2; c < cMax; c += 2) {
+		assert(c - center !== -D && c - center !== D);
+		const xp = V[c + 1];
+		const yp = V[c - 1];
+		V[c] = xp > yp ? yp : xp - 1;
 	}
 
 	if (UB !== LB) {
-		const k = UB;
-		const xp = V[cx + k];
-		const yp = V[cy + k];
-		assert(k === D || xp > yp);
-		V[center + k] = yp;
+		assert(c - center === UB);
+		const xp = V[c + 1];
+		const yp = V[c - 1];
+		assert(c - center === D || xp > yp);
+		V[c] = yp;
 	}
 
 	console.debug('end backwardStep', {

commit 17bf009306f27b06bc2bd7fb5723c9a7830cd0e4

:bicyclist: perf(backwardStep): Revert to previous faster implementation.
Reverts implementation + backports argument elimination.
The version restored is ff2a6f8^ = f6bb86d0ff2a165a7933c1775f4b3fb61bfc33cc.

This is for further reference. I am still trying to understand if the
slowdown introduced by ff2a6f8 is microbundle's fault or V8's fault.

Is it slow because:
    - We use a subtraction instead of two additions (some pipeline crap).
    - c is not scoped to the hot loop.
    - something outside the hot loop.

src/backwardStep.js
@@ -35,27 +35,27 @@ export default function backwardStep(center, D, V, li, lj, ri, rj) {
 		UB,
 	});
 
-	let c = center + LB;
-	const cMax = center + UB;
+	const cx = center + 1;
+	const cy = center - 1;
 
-	assert(c - center === LB);
-	const xp = V[c + 1];
-	const yp = V[c - 1];
-	V[c] = LB !== -D && xp > yp ? yp : xp - 1;
+	const k = LB;
+	const xp = V[cx + k];
+	const yp = V[cy + k];
+	V[center + k] = k !== -D && xp > yp ? yp : xp - 1;
 
-	for (c += 2; c < cMax; c += 2) {
-		assert(c - center !== -D && c - center !== D);
-		const xp = V[c + 1];
-		const yp = V[c - 1];
-		V[c] = xp > yp ? yp : xp - 1;
+	for (let k = LB + 2; k < UB; k += 2) {
+		assert(k !== -D && k !== D);
+		const xp = V[cx + k];
+		const yp = V[cy + k];
+		V[center + k] = xp > yp ? yp : xp - 1;
 	}
 
 	if (UB !== LB) {
-		assert(c - center === UB);
-		const xp = V[c + 1];
-		const yp = V[c - 1];
-		assert(c - center === D || xp > yp);
-		V[c] = yp;
+		const k = UB;
+		const xp = V[cx + k];
+		const yp = V[cy + k];
+		assert(k === D || xp > yp);
+		V[center + k] = yp;
 	}
 
 	console.debug('end backwardStep', {