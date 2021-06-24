9e0884f0293786c1aeb4db0f5379cdbc49cd6235
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-06-24 21:01:44 +0200

    :bicyclist: perf(boundAlloc): Remove unnecessary Math.max and document output.

--- src/boundAlloc.js
+++ src/boundAlloc.js
@@ -1,12 +1,14 @@
+import assert from 'assert';
+
 /**
  * BoundAlloc.
- *
- * TODO Remove Math.max(..., 1) since we skip D=0 now?
  *
  * @param {number} MAX
  * @param {number} li
  * @param {number} lj
+ * @return {number}
  */
-const boundAlloc = (MAX, li, lj) => Math.min(MAX, Math.max(lj - li, 1));
-
-export default boundAlloc;
+export default function boundAlloc(MAX, li, lj) {
+	assert(lj > li);
+	return Math.min(MAX, lj - li);
+}
