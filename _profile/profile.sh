d1701d6c7d484f14e4f9023c3d6a6c6101ad32c3
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-06-24 16:58:41 +0200

    :sparkles: feat(_profile): Add TIMESTAMP and use v8.log extension in filename.

--- _profile/profile.sh
+++ _profile/profile.sh
@@ -6,6 +6,8 @@ L="$3"
 D="$4"
 I="$5"
 
-logfile="${main}-n=${n}-L=${L}-D=${D}-I=${I}.log"
+TIMESTAMP="$(date '+%Y-%m-%dT%H:%M:%S')"
+
+logfile="${main}-n=${n}-L=${L}-D=${D}-I=${I}-${TIMESTAMP}-v8.log"
 
 node --prof --logfile="$logfile" "$@"
